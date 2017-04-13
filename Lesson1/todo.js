dc5f4294364e7fdd814ef106637caef07792fe07
Byron Hambly <byron@clockboot.com>
2017-04-13 10:51:41 +0200

    we started a todo app!

--- Lesson1/todo.js
+++ Lesson1/todo.js
@@ -1,4 +1,25 @@
-var app = angular.module('', []);
+var app = angular.module('todoApp', []);
 
-app.controller('', ["$scope", function ($scope) {
+app.controller('todoController', ["$scope", function ($scope) {
+  $scope.todos = [
+    { text: 'one', done: false }, 
+    { text: 'two', done: false }
+  ];
+
+  $scope.newTodo = '';
+
+  $scope.addTodo = function() {
+    $scope.todos.push(
+      { text: $scope.newTodo, done: false }
+    );
+    $scope.newTodo = '';
+  }
+
+  $scope.clearDone = function() {
+    $scope.todos = $scope.todos.filter(function(todo) {
+      if (!todo.done) {
+        return todo;
+      }
+    });
+  }
 }]);
